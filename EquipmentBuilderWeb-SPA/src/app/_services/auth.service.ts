0496418a0ef2bf2fd4ffa0c07bb0bb5ef4d14ba1
Nepetun <mariuszchalczynski@gmail.com>
2020-02-10 23:36:18 +0100

    logowanie Angular
    - możliwośc logowania
    - przechowywanie tokenu klienta
    - wylogowywanie

---
+++ EquipmentBuilderWeb-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'// który moduł może skorzystać z serwisu - dla nas jest to appmodule
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+
+constructor(private http: HttpClient) { }
+
+/* metoda logowania - tworzymy post tak jak za pomocą postmana
+do metody login - co przesyłamy?
+przesyłamy nasz model - który będzie z naszej formy angularowej
+następnie musimy użyć pipe - ze wzgledu na to ze serwer zwroci token
+musimy go przekazac za pomoca pipe - dla observable
+i przetwarzamy go za pomoca map
+mapujemy response - jako jakikolwiek typ
+map bo klucz i wartosc
+jezeli user istnieje to ustawiamy localstorage na jego token
+*/
+login(model: any) {
+  return this.http.post(this.baseUrl + 'login' , model)
+  .pipe(
+    map((reponse: any) => {
+      const user = reponse;
+      if (user) {
+        localStorage.setItem('token', user.token);
+      }
+    })
+  );
+}
+
+}
---
+++ EquipmentBuilderWeb-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { logging } from 'protractor';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+ model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    // ze wzgledu na to ze metoda login jest observable uzywamy subscribe
+    this.authService.login(this.model).subscribe(next => {
+        console.log('logged in sucesfully');
+      }, error => {
+        console.log('failed to login...');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token; // !! zwraca tru or false -> true jezeli jest token, false jezeli jest pusty
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out :c');
+  }
+}
